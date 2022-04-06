766697979bc5c5d5a6feb45bb568733f7bda6b53
JON ROBERT ARMSTRONG <JON.ROBERT.ARMSTRONG@GMAIL.COM>
2022-04-05 22:29:51 -0700

    FIXED MAJOR BUGS CAUSED BY TYPOS AND SYNTAX ERRORS.  ADDED MORE LOGS AND NOTES

--- js/app.js
+++ js/app.js
@@ -15,8 +15,11 @@ if (hourNow > 18) {
 }else if (hourNow > 0) {
     greeting = 'Good morning, ';
 }else {
-    greeting = 'Welcome '
+    greeting = 'Welcome ';
 }
+
+console.log(`The hour right now is ${hourNow}.`);
+console.log(`${greeting} was the greeting provided.`);
 
 // About me
 
@@ -24,9 +27,12 @@ let visitor = prompt(`Hello.  I'm Jon (he/him).  What's your Name?`);
 
 alert(`${greeting} ${visitor}, let's play a guessing game.  Please answer all questions yes or no.`);
 
-// Question one
+console.log(`The name "${visitor}" was provided.`);
 
-let qOne = prompt('Have I ever been married?','yes').toLocaleLowerCase();
+// Question one
+// Changed string to lower case at entry into variable
+
+let qOne = prompt('Have I ever been married?','yes').toLowerCase();
 
 if(qOne === 'yes' || qOne === 'y'){
     alert('Winner! Winner!');
@@ -36,40 +42,51 @@ if(qOne === 'yes' || qOne === 'y'){
     alert('I didn\'t understand that response.');
 }
 
-// Question two
+console.log(`${qOne} was recieved for the first question.`);
 
-let qTwo = prompt('Have I ever lived in Oregon?','no').toLocaleUpperCase;
+// Question two
+// Changed string to upper case at entry into variable
+
+let qTwo = prompt('Have I ever lived in Oregon?','no').toUpperCase();
 
 if(qTwo === 'YES' || qTwo === 'Y'){
-    alert('Winner! Winner!');
-}else if (qTwo === 'NO' || qTwo === 'N'){
     alert('Nope!');
+}else if (qTwo === 'NO' || qTwo === 'N'){
+    alert('Winner! Winner!');
 }else {
     alert('I didn\'t understand that response.');
 }
 
-//Question three
+console.log(`${qTwo} was recieved for the first question.`);
+
+// Question three
+// Changed string to lower case in conditional statement to preserve string entered by user
+// Preserving the string consumed four times the amount of characters in coding.
 
 let qThree = prompt('Have I ever shot a man?', 'no');
 
-if(qThree.toLocaleLowerCase === 'no' || qThree.toLocaleLowerCase === 'n'){
+if(qThree.toLowerCase() === 'no' || qThree.toLowerCase() === 'n'){
     alert('Winner! Winner!');
-}else if (qThree.toLocaleLowerCase === 'yes' || qThree.toLocaleLowerCase === 'y'){
+}else if (qThree.toLowerCase() === 'yes' || qThree.toLowerCase() === 'y'){
     alert('Nope!');
 }else {
     alert('I didn\'t understand that response.');
 }
 
-//Question four
+console.log(`${qThree} was recieved for the first question.`);
+
+// Question four
+// Changed string to lower case in conditional statement to preserve string entered by user
+// Rearanged and reduced conditional staement using parentheses and deleting spaces and breaks.
+// This effort only provided a minor reduction in coding characters but reduced lines or code by almost half.
 
 let qFour = prompt('Did I walk all the way from Mexico to Canada?', 'no');
 
-if(qFour.toLocaleLowerCase === ('no') || ('n')){
-    alert('Winner! Winner!');
-}else if (qFour.toLocaleLowerCase === ('yes' || 'y')){
-    alert('Nope!');
-}else {
-    alert('I didn\'t understand that response.');
+if(qFour.toLowerCase() === ('no') || ('n')){alert('Winner! Winner!');
+}else if (qFour.toLowerCase() === ('yes' || 'y')){alert('Nope!');
+}else {alert('I didn\'t understand that response.');
 }
 
-console.log('Hello.  Your script completed.')
+console.log(`${qFour} was recieved for the first question.`);
+
+console.log('Hello.  Your script completed.');
